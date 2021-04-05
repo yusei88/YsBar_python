613cf60cd14c026f99ae49f9d041541bfe8627cb
yusei <yuseisato1@gmail.com>
2021-04-05 10:25:40 +0900

    bootstrap適用,menuページ作成

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = "Hello World"
-    return render_template('top.html', title="トップページ", name="SATO")
+    return render_template('index.html', title="Y'sBAR")
+
+@app.route('/gin')
+def menu():
+    return render_template('gin.html', title="gin")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
